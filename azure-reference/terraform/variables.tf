99ce571dc30dd64aef86e1188cc1f9918f1cffba
OBINNA OBIKA <bilolo74@gmail.com>
2026-09-21 00:23:02 -0400

    Add azure-reference/terraform/variables.tf

---
+++ azure-reference/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  description = "Resource naming prefix."
+  type        = string
+  default     = "portfolio-platform"
+}
+
+variable "location" {
+  description = "Azure region."
+  type        = string
+  default     = "eastus"
+}
